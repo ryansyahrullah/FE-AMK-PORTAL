d2ce3099bda297aa64134d2b50da98157fdb548b
Ryan Syahrullah <syahrullahryan@gmail.com>
2025-10-01 12:54:09 +0800

    Set login page as root route

---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import LoginPage from '../components/LoginPage.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'login',
+    component: LoginPage,
+    alias: ['/login'],
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    redirect: '/',
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+})
+
+export default router
